822a9b8b8e59e7749df92c6fae5ac40ca91a515f
dayviddouglas <dayvidacademico@gmail.com>
2023-12-14 17:05:02 -0300

    Impleting style of dados e perfil.

--- screens/dados/index.js
+++ screens/dados/index.js
@@ -1,5 +1,5 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View, ScrollView, Pressable, TextInput, Button, Alert } from 'react-native';
-import { Horse, ArrowLeft, DotsThreeOutline, Equals, TextAlignJustify } from 'phosphor-react-native';
+import { Horse, ArrowLeft, DotsThreeOutline, Equals, TextAlignJustify, EnvelopeSimple, Phone } from 'phosphor-react-native';
 import fotoPerfil from "../../assets/categorias/Alessandro-Henrique.png";
 import { useNavigation } from '@react-navigation/native'
 import axios from 'axios';
@@ -57,96 +57,154 @@ export const Dados = ({route}) => {
       </Pressable>
     </View>
 
-      <View style={styles.caixaPerfil}>
 
-      <Image style={styles.fotoPerfil} src={foto} />
-      <Text style={[styles.texto, styles.negrito]}>{nome}</Text>
-      <Text style={styles.texto}>CPF: {cpf}</Text>
-      <TextInput defaultValue={email} onChangeText={(text)=>{
+
+    <View style={styles.caixaPerfil}>
+
+      <View style={styles.caixaPrincipal}>
+        <Image style={styles.fotoPerfil} src={foto} />
+        <View style={styles.caixaDireita}>
+        <Text style={[styles.texto, styles.negrito, {marginBottom: 10}]}>{nome}</Text>
+        <Text style={styles.texto}>CPF: {cpf}</Text>
+        </View>
+      </View>
+
+      <Text style={[styles.texto, {marginBottom: 30, fontWeight: "bold", fontSize: 30}]}>Atualize seu email e telefone:</Text>
+
+      <View style={{justifyContent:'center'}}>
+      <View style={styles.caixaSegundario}>
+        <EnvelopeSimple color='#fff' />
+        <TextInput style={[styles.texto, styles.textoImput, {marginLeft: 20}]} defaultValue={email} onChangeText={(text)=>{
           setEmailAtual(text)
       }}/>
-      <TextInput defaultValue={telefone} onChangeText={(text)=>{
+      </View>
+      <View style={styles.caixaSegundario}>
+        <Phone color='#fff' />
+        <TextInput style={[styles.texto, styles.textoImput, {marginLeft: 20, marginBottom: 50}]} defaultValue={telefone} onChangeText={(text)=>{
         setTelefoneAtual(text)
       }}/>
+      </View>
+      </View>
+
+
+
       <Button fontSize= "50"
-          title="Atualizar Perfil"
-          color="#73B6B6" 
+          title= "Atualizar Perfil"
+          color= "#73B6B6"
           onPress={()=>{
             updatePerfil()
           }}
           />
-      
-    
-      {/* <Text style={styles.texto}>EMAIL: {email}</Text>
-      <Text style={styles.texto}>TELEFONE: {telefone}</Text> */}
+
       </View>
+
+      
   
     </View>
     )
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#DF8D2D',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#DF8D2D',
+  },
 
-    header: {
-      flexDirection: 'row',
-      width: "100 %",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: 80,
-      paddingHorizontal: 35
-    },
+  header: {
+    flexDirection: 'row',
+    width: "100 %",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 80,
+    paddingHorizontal: 35
+  },
 
-    logo:{
-      marginTop: 0,
-      width: "50 %",
-      height: "500 %",
-    },
+  logo:{
+    marginTop: 0,
+    width: "50 %",
+    height: "500 %",
+  },
 
-    caixaPerfil:{
-      marginTop: 10,
-      marginLeft: 10,
-      marginRight: 10,
-      marginBottom: 30,
-    },
+  caixaPerfil:{
+    flexDirection: 'row',
+  },
 
-    listaCategoria: {
-      margin: 0,
-    },
+  caixaPerfil:{
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 30,
+  },
 
-    botoes: {
-      color: '#DF8D2D',
-    },
+  listaCategoria: {
+    margin: 0,
+  },
 
-    fotoPerfil: {
-      width: 200,
-      height: 200,
-      alignSelf: "center",
-      borderRadius:200,
-    },
+  botoes: {
+    color: '#DF8D2D',
+  },
 
-    texto:{
-      textAlign: "center",
-      color: "#fff",
-      fontSize: 20,
-    },
-    
-    negrito: {
-      fontWeight: "bold",
-      fontSize: 35,
-    },
+  caixaPrincipal:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginBottom: 30,
+    borderColor: '#fff',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    paddingBottom: 20,
+    paddingTop: 20
+  },
 
-    opcoes: {
-      backgroundColor: "#fff",
-      alignItems:"center",
-      alignSelf: "center",
-      marginBottom: 20,
-      height: 50,
-      width: "50 %",
-      borderRadius:20,
-      justifyContent: "center",
-    },
-  });
+  caixaDireita:{
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+
+  fotoPerfil: {
+    width: 150,
+    height: 150,
+    alignSelf: "center",
+    borderRadius:200,
+  },
+
+  caixaSegundario:{
+    flexDirection: 'row',
+    marginLeft: 30,
+    marginTop: 5,
+  },
+
+  texto:{
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+  },
+
+  textoImput:{
+    textAlign: "center",
+    color: "#000",
+    fontSize: 22,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    width: '100%',
+    maxWidth: 300,
+  },
+
+  negrito: {
+    fontWeight: "bold",
+    fontSize: 35,
+    maxWidth: 250,
+    textAlign: "center",
+  },
+
+  opcoes: {
+    backgroundColor: "#fff",
+    alignItems:"center",
+    alignSelf: "center",
+    marginBottom: 20,
+    height: 50,
+    width: "50 %",
+    borderRadius:20,
+    justifyContent: "center",
+  },
+});
--- screens/perfil/index.js
+++ screens/perfil/index.js
@@ -1,5 +1,5 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View, ScrollView, Pressable } from 'react-native';
-import { Horse, ArrowLeft, DotsThreeOutline, Equals, TextAlignJustify } from 'phosphor-react-native';
+import { Horse, ArrowLeft, DotsThreeOutline, Equals, TextAlignJustify, EnvelopeSimple, Phone } from 'phosphor-react-native';
 import fotoPerfil from "../../assets/categorias/Alessandro-Henrique.png";
 import { useNavigation } from '@react-navigation/native'
 import axios from 'axios';
@@ -9,6 +9,7 @@ import { Dados } from '../dados';
 import React from 'react'
 import { CardLojas } from '../../utils/cardCategoria';
 import {IPLOCAL} from "@env";
+
 
 export const Perfil = ({route}) => {
   const navigation = useNavigation();
@@ -40,6 +41,38 @@ const sairLogin = () =>{
       </Pressable>
     </View>
 
+      
+      <View style={styles.caixaPerfil}>
+      <View style={styles.caixaPrincipal}>
+        <Image style={styles.fotoPerfil} src={foto} />
+        <View style={styles.caixaDireita}>
+        <Text style={[styles.texto, styles.negrito, {marginBottom: 10}]}>{nome}</Text>
+        <Text style={styles.texto}>CPF: {cpf}</Text>
+        </View>
+      </View>
+
+      <View style={styles.caixaSegundario}>
+        <EnvelopeSimple color='#fff' />
+        <Text style={[styles.texto, {marginLeft: 20}]}>{email}</Text>
+      </View>
+      <View style={styles.caixaSegundario}>
+        <Phone color='#fff' />
+        <Text style={[styles.texto, {marginLeft: 20}]}>{telefone}</Text>
+      </View>
+      </View>
+
+      <View style={styles.caixaOpcoes}>
+        <Pressable style={styles.opcoes} onPress={()=>{navigation.navigate('Dados',{
+          loginUser:login
+        })}} >
+          <Text style={[styles.negrito, {fontSize: 20}]}>Atualizar dados</Text>
+        </Pressable>
+        <Pressable style={styles.opcoes} onPress={sairLogin}>
+          <Text style={[styles.negrito, {fontSize: 20}]} >Sair</Text>
+        </Pressable>
+      </View>
+
+{/*  
       <View style={styles.caixaPerfil}>
       <Image style={styles.fotoPerfil} src={foto} />
       <Text style={[styles.texto, styles.negrito]}>{nome}</Text>
@@ -57,73 +90,102 @@ const sairLogin = () =>{
         <Pressable style={styles.opcoes} onPress={sairLogin}>
           <Text style={[styles.negrito, {fontSize: 20}]}>Sair</Text>
         </Pressable>
-      </View>
-    </View>
+      </View> */}
+    </View>  
     )
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#DF8D2D',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#DF8D2D',
+  },
 
-    header: {
-      flexDirection: 'row',
-      width: "100 %",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: 80,
-      paddingHorizontal: 35
-    },
+  header: {
+    flexDirection: 'row',
+    width: "100 %",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 80,
+    paddingHorizontal: 35
+  },
 
-    logo:{
-      marginTop: 0,
-      width: "50 %",
-      height: "500 %",
-    },
+  logo:{
+    marginTop: 0,
+    width: "50 %",
+    height: "500 %",
+  },
 
-    caixaPerfil:{
-      marginTop: 10,
-      marginLeft: 10,
-      marginRight: 10,
-      marginBottom: 30,
-    },
+  caixaPerfil:{
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 30,
+  },
 
-    listaCategoria: {
-      margin: 0,
-    },
+  listaCategoria: {
+    margin: 0,
+  },
 
-    botoes: {
-      color: '#DF8D2D',
-    },
+  botoes: {
+    color: '#DF8D2D',
+  },
 
-    fotoPerfil: {
-      width: 200,
-      height: 200,
-      alignSelf: "center",
-      borderRadius:200,
-    },
+  caixaPrincipal:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginBottom: 30,
+    borderColor: '#fff',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    paddingBottom: 20,
+    paddingTop: 20
+  },
 
-    texto:{
-      textAlign: "center",
-      color: "#fff",
-      fontSize: 20,
-    },
-    
-    negrito: {
-      fontWeight: "bold",
-      fontSize: 35,
-    },
+  caixaDireita:{
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
 
-    opcoes: {
-      backgroundColor: "#fff",
-      alignItems:"center",
-      alignSelf: "center",
-      marginBottom: 20,
-      height: 50,
-      width: "50 %",
-      borderRadius:20,
-      justifyContent: "center",
-    },
-  });
+  fotoPerfil: {
+    width: 150,
+    height: 150,
+    alignSelf: "center",
+    borderRadius:200,
+  },
+
+  caixaSegundario:{
+    flexDirection: 'row',
+    marginLeft: 30,
+    marginTop: 5
+  },
+
+  texto:{
+    // textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+  },
+
+  negrito: {
+    fontWeight: "bold",
+    fontSize: 35,
+    maxWidth: 250,
+    textAlign: "center",
+  },
+
+  caixaOpcoes: {
+    marginTop: 40
+  },
+
+  opcoes: {
+    backgroundColor: "#fff",
+    alignItems:"center",
+    alignSelf: "center",
+    marginBottom: 20,
+    height: 50,
+    width: "50 %",
+    borderRadius:20,
+    justifyContent: "center",
+  },
+});
